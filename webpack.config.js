e734345ba206ba651eec7dffb61e8e91c9122416
yukisa2010 <43121712+yukisa2010@users.noreply.github.com>
2019-11-09 22:26:10 +0900

    1109 updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 // const HtmlWebpackPlugin = require('html-webpack-plugin')
 // const webpack = require('webpack')
+//command npm run dev
 
 module.exports = {
   mode: "development",
